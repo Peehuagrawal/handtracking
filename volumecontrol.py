7076ae591d5012743aa8e257fc8b7dcbca926041
Peehu_agrawal <166029190+Peehuagrawal@users.noreply.github.com>
2025-06-17 17:53:07 +0530

    initial commit

--- volumecontrol.py
+++ volumecontrol.py
@@ -1,12 +1,43 @@
 import cv2
 import mediapipe as mp
 import time
+import HandTrackingModule as htm
+import math
 
 cap = cv2.VideoCapture(0)  # Try 1 if 0 doesn't work
 
+wCam, hCam= 640, 480
+
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime=0
+
+detector= htm.handDetector()
 
 while True:
     success, img = cap.read()
+    img= detector.findHands(img)
+    lmlist=detector.findPosition(img, draw=False)
+    if len(lmlist)!=0:
+        #print(lmlist[4],lmlist[8])
+        x1, y1= lmlist[4][1], lmlist[4][2]
+        x2, y2= lmlist[8][1], lmlist[8][2]
+        cx,cy= (x1+x2)//2, (y1+y2)//2
+
+        cv2.circle(img, (x1,y1), 10, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x2,y2), 10, (255,0,255), cv2.FILLED)
+        cv2.line(img, (x2,y2), (x1,y1), (255,0,255), 3)
+        cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED)
+            
+        length=math.hypot(x2-x1,y2-y1)
+        print(length)
+
+
+    cTime= time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+
+    cv2.putText(img, str(int(fps)),(40,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
     
     cv2.imshow("Image", img)
 
